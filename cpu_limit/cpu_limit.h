569edd1acd960cd0e0c77aa178437852cf9503dd
Kun He <kun.he@cs2c.com.cn>
2014-06-19 10:39:13 +0800

    Update doxygen files.

--- cpu_limit/cpu_limit.h
+++ cpu_limit/cpu_limit.h
@@ -1,10 +1,37 @@
+//////////////////////////////////////////////////////////////////////////
+/// COPYRIGHT NOTICE
+/// Copyright(c) 2014, CS2C
+/// All rights reserved.
+///
+/// @file       cpu_limit.h
+/// @brief      Head file for cpu_limit.c
+///
+/// @version    0.1
+/// @author     Kun He(kun.he@cs2c.com.cn)
+/// @date       2014.06.19
+///
+//////////////////////////////////////////////////////////////////////////
 typedef struct CPU_UTILIZATION 
 {
     int     cpu;
     double   utilization;
 } cpu_utilization;
-
-void * thread_bind_cpu(void *);
+/// @brief  Bind a thread with a utilization percentage.
+/// 
+/// <pre>
+/// This function should be called by pthread_create. First define 
+/// a cpu_utilization point, and convert it to void*.
+/// Example:
+///     pthread_t * p_tid; 
+///     cpu_utilization * cpu_args;
+///     pthread_create(p_tid, NULL, thread_bind_cpu, (void*)(cpu_args));
+/// </pre>
+/// @param[in] arg void *  The cpu number and it's utilization percentage.
+/// @return void *
+/// @retval N/A
+/// @see
+/// @notice
+void * thread_bind_cpu(void * cpu_args);
 int multi_threads_run(cpu_utilization *, int);
 int get_percentage_value(double *);
 int set_cpu_percentage(cpu_utilization *, int);
--- cpu_limit/memory_limit.h
+++ cpu_limit/memory_limit.h
@@ -1,8 +1,17 @@
-/*++
- * wulw, jianyaoshuoming.
- * Get the free space size of system memory.
- * @return  type: long value: the free space.   
---*/
+/////////////////////////////////////////////
+/// COPYRIGHT NOTICE
+/// Copyright(c) 2014, CS2C
+/// All rights reserved.
+///
+/// @file       memory_limit.h
+/// @brief      Head file for memory_limit.c
+///
+/// @version    0.1
+/// @author     Kun He(kun.he@cs2c.com.cn)
+/// @date       2014.06.19
+///
+////////////////////////////////////////////// 
+
 long free_mem();
 int drop_cache(char *);
 int set_opt(char *, long *, long *);
